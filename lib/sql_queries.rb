b0b2243d6a43b597c649becc96c29bcd55c5f0a4
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-26 00:08:56 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_title
-  "SELECT owners.name, SUM(cats.net_worth)
+  "SELECT owners.name
    FROM owners
    INNER JOIN cats_owners
    ON owners.id = cats_owners.owner_id
