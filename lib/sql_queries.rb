17fb93bbad193912f5d4935b063b44884dcd74ec
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-26 01:06:40 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -49,7 +49,8 @@ def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_ca
   "SELECT projects.category, pledges.amount
   FROM projects 
   JOIN pledges 
-  ON projects.id = pledges.
+  ON projects.id = pledges.project_id 
+  WHERE projects.category = 'music'; 
   "
 end
 
