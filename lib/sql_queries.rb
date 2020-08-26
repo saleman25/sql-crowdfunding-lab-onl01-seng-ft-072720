52e5b79da47fd3e7675f9b6d1304106cb41d7cc3
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-26 00:45:06 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -17,8 +17,8 @@ end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
   "SELECT users.name, users.age, SUM(pledges.amount)
-  FROM pledges 
-  LEFT JOIN users 
+  FROM users 
+  LEFT JOIN pledges 
   ON users.id = pledges.user_id
   GROUP BY users.name
   ORDER BY SUM(pledges.amount);
